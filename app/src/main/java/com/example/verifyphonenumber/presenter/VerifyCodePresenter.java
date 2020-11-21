4a2b7e392147c1b0b40dfcd4c2001b34511cb7d9
Aram <arambaghdas@gmail.com>
2020-11-21 20:07:54 +0400

    Minor changes

--- app/src/main/java/com/example/verifyphonenumber/presenter/VerifyCodePresenter.java
+++ app/src/main/java/com/example/verifyphonenumber/presenter/VerifyCodePresenter.java
@@ -9,18 +9,18 @@ import com.example.verifyphonenumber.views.VerifyView;
 public class VerifyCodePresenter {
 
     private Activity activity;
-    private VerifyView baseView;
+    private VerifyView verifyView;
     private boolean firstTime = true;
 
-    public VerifyCodePresenter(Activity activity, VerifyView baseView) {
+    public VerifyCodePresenter(Activity activity, VerifyView verifyView) {
         this.activity = activity;
-        this.baseView = baseView;
+        this.verifyView = verifyView;
     }
 
     public void verifyPhoneNumber(String number) {
         if (firstTime) {
             firstTime = false;
-            baseView.disableRegistration();
+            verifyView.disableRegistration();
         } else {
             checkPhoneNumber(getPhoneNumber(number));
         }
@@ -30,26 +30,26 @@ public class VerifyCodePresenter {
         Log.v("numberWithCode", "numberWithCode: " + number);
         if (!number.isEmpty()) {
             if (number.length() > 3 && !(number.startsWith("+79"))) {
-                baseView.showErrorView();
-                baseView.showErrorResponse(activity.getString(R.string.should_start_with_seven_nine));
-                baseView.disableRegistration();
+                verifyView.showErrorView();
+                verifyView.showErrorResponse(activity.getString(R.string.should_start_with_seven_nine));
+                verifyView.disableRegistration();
             } else if (!number.matches("[0-9+]+")) {
-                baseView.showErrorView();
-                baseView.showErrorResponse(activity.getString(R.string.should_contain_number));
-                baseView.disableRegistration();
+                verifyView.showErrorView();
+                verifyView.showErrorResponse(activity.getString(R.string.should_contain_number));
+                verifyView.disableRegistration();
             } else if (number.length() < 12) {
-                baseView.disableRegistration();
-                baseView.showErrorResponse("");
-                baseView.hideErrorView();
+                verifyView.disableRegistration();
+                verifyView.showErrorResponse("");
+                verifyView.hideErrorView();
             } else {
-                baseView.showErrorResponse("");
-                baseView.hideErrorView();
-                baseView.enableRegistration();
+                verifyView.showErrorResponse("");
+                verifyView.hideErrorView();
+                verifyView.enableRegistration();
             }
         } else {
-            baseView.disableRegistration();
-            baseView.showErrorResponse("");
-            baseView.hideErrorView();
+            verifyView.disableRegistration();
+            verifyView.showErrorResponse("");
+            verifyView.hideErrorView();
         }
     }
 
